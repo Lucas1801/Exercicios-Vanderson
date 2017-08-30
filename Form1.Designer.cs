873d08c2a3cb85cb6299fec368acbc601b82628d
Lucas1801 <l.r.chaves01@gmail.com>
2017-08-29 22:20:09 -0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,227 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.txttabuada = new System.Windows.Forms.TextBox();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.btnExercicio5 = new System.Windows.Forms.Button();
+            this.btnExercicio2 = new System.Windows.Forms.Button();
+            this.btnExercicio3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.txtExercicio4 = new System.Windows.Forms.TextBox();
+            this.btnExercicio4 = new System.Windows.Forms.Button();
+            this.txtP = new System.Windows.Forms.TextBox();
+            this.txtV = new System.Windows.Forms.TextBox();
+            this.btnExercicio7 = new System.Windows.Forms.Button();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(118, 32);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Exercicio6";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txttabuada
+            // 
+            this.txttabuada.Location = new System.Drawing.Point(118, 6);
+            this.txttabuada.Name = "txttabuada";
+            this.txttabuada.Size = new System.Drawing.Size(73, 20);
+            this.txttabuada.TabIndex = 1;
+            this.txttabuada.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txttabuada_KeyPress);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(1, 1);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(371, 260);
+            this.tabControl1.TabIndex = 2;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.btnExercicio5);
+            this.tabPage1.Controls.Add(this.btnExercicio2);
+            this.tabPage1.Controls.Add(this.btnExercicio3);
+            this.tabPage1.Controls.Add(this.button2);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(363, 234);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // btnExercicio5
+            // 
+            this.btnExercicio5.Location = new System.Drawing.Point(7, 154);
+            this.btnExercicio5.Name = "btnExercicio5";
+            this.btnExercicio5.Size = new System.Drawing.Size(75, 23);
+            this.btnExercicio5.TabIndex = 3;
+            this.btnExercicio5.Text = "Exercicio5";
+            this.btnExercicio5.UseVisualStyleBackColor = true;
+            this.btnExercicio5.Click += new System.EventHandler(this.btnExercicio5_Click);
+            // 
+            // btnExercicio2
+            // 
+            this.btnExercicio2.Location = new System.Drawing.Point(7, 67);
+            this.btnExercicio2.Name = "btnExercicio2";
+            this.btnExercicio2.Size = new System.Drawing.Size(75, 23);
+            this.btnExercicio2.TabIndex = 2;
+            this.btnExercicio2.Text = "Exercicio2";
+            this.btnExercicio2.UseVisualStyleBackColor = true;
+            this.btnExercicio2.Click += new System.EventHandler(this.btnExercicio2_Click);
+            // 
+            // btnExercicio3
+            // 
+            this.btnExercicio3.Location = new System.Drawing.Point(7, 114);
+            this.btnExercicio3.Name = "btnExercicio3";
+            this.btnExercicio3.Size = new System.Drawing.Size(75, 23);
+            this.btnExercicio3.TabIndex = 1;
+            this.btnExercicio3.Text = "Exercicio3";
+            this.btnExercicio3.UseVisualStyleBackColor = true;
+            this.btnExercicio3.Click += new System.EventHandler(this.btnExercicio3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(7, 28);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "Exercicio 1";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.btnExercicio7);
+            this.tabPage2.Controls.Add(this.txtV);
+            this.tabPage2.Controls.Add(this.txtP);
+            this.tabPage2.Controls.Add(this.txtExercicio4);
+            this.tabPage2.Controls.Add(this.btnExercicio4);
+            this.tabPage2.Controls.Add(this.txttabuada);
+            this.tabPage2.Controls.Add(this.button1);
+            this.tabPage2.ForeColor = System.Drawing.Color.Maroon;
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(363, 234);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            this.tabPage2.Click += new System.EventHandler(this.tabPage2_Click);
+            // 
+            // txtExercicio4
+            // 
+            this.txtExercicio4.Location = new System.Drawing.Point(11, 6);
+            this.txtExercicio4.Name = "txtExercicio4";
+            this.txtExercicio4.Size = new System.Drawing.Size(70, 20);
+            this.txtExercicio4.TabIndex = 3;
+            this.txtExercicio4.TextChanged += new System.EventHandler(this.txtExercicio4_TextChanged);
+            // 
+            // btnExercicio4
+            // 
+            this.btnExercicio4.Location = new System.Drawing.Point(6, 32);
+            this.btnExercicio4.Name = "btnExercicio4";
+            this.btnExercicio4.Size = new System.Drawing.Size(75, 23);
+            this.btnExercicio4.TabIndex = 2;
+            this.btnExercicio4.Text = "Exercicio4";
+            this.btnExercicio4.UseVisualStyleBackColor = true;
+            this.btnExercicio4.Click += new System.EventHandler(this.btnExercicio4_Click);
+            // 
+            // txtP
+            // 
+            this.txtP.Location = new System.Drawing.Point(281, 6);
+            this.txtP.Name = "txtP";
+            this.txtP.Size = new System.Drawing.Size(81, 20);
+            this.txtP.TabIndex = 4;
+            // 
+            // txtV
+            // 
+            this.txtV.Location = new System.Drawing.Point(281, 32);
+            this.txtV.Name = "txtV";
+            this.txtV.Size = new System.Drawing.Size(81, 20);
+            this.txtV.TabIndex = 5;
+            // 
+            // btnExercicio7
+            // 
+            this.btnExercicio7.Location = new System.Drawing.Point(281, 58);
+            this.btnExercicio7.Name = "btnExercicio7";
+            this.btnExercicio7.Size = new System.Drawing.Size(81, 23);
+            this.btnExercicio7.TabIndex = 6;
+            this.btnExercicio7.Text = "Exercicio7";
+            this.btnExercicio7.UseVisualStyleBackColor = true;
+            this.btnExercicio7.Click += new System.EventHandler(this.btnExercicio7_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 262);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txttabuada;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button btnExercicio3;
+        private System.Windows.Forms.Button btnExercicio2;
+        private System.Windows.Forms.Button btnExercicio5;
+        private System.Windows.Forms.Button btnExercicio4;
+        private System.Windows.Forms.TextBox txtExercicio4;
+        private System.Windows.Forms.Button btnExercicio7;
+        private System.Windows.Forms.TextBox txtV;
+        private System.Windows.Forms.TextBox txtP;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,208 @@
+﻿//Lucas Rodrigues Chaves 1601259
+//Ronaldo de souza santos 1600715
+//Raphael Rodrigues 1510203
+//Leando Tadeu 1600328
+//Isak Silva 1600364
+//Wesley Saldanha Rosa 141198
+
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+          public Form1()
+        {
+            InitializeComponent();
+        }
+       
+          public void Exercicio1()
+          {
+              int soma = 0;
+              for (int i = 0; i <= 100; i++)
+              {
+                  soma = soma + i;
+              }
+              MessageBox.Show("Resultado da soma dos numeros de 1 a 100 é: " + soma);
+          }
+        //--------------------------------------------------------------------
+          public void Exercicio2()
+          {
+              //exercicio 2
+
+              int resultado;
+            String menssagem = String.Empty;
+              for (int j = 1; j <= 200; j++)
+              {
+                  resultado = 7 * j;
+                  if (resultado < 200)
+                  {
+                    menssagem += (resultado).ToString() + Environment.NewLine;
+                }
+
+              }
+            MessageBox.Show(menssagem);
+        }
+        //--------------------------------------------------------------------
+          public void Exercicio3()
+          {
+           
+            //exercicio 3 
+            int numero;
+            String menssagem = String.Empty;
+            for (numero = 1; numero <= 100; numero++)
+            {
+                if (numero % 4 == 0)
+                    menssagem += (numero).ToString() + Environment.NewLine;
+            }
+           
+            MessageBox.Show(menssagem);
+        }
+        //-------------------------------------------------------------------
+          public void Exercicio4()
+          {
+              double resultado = 0;
+              double numeroUsuario = Convert.ToInt32(txtExercicio4.Text);
+              int sinal = 1;
+
+              for (int i = 1; i <= 3; i++)
+              {
+                  resultado += sinal * (numeroUsuario / i);
+                  sinal *= -1;
+                  MessageBox.Show(Convert.ToString(resultado));  
+              }
+              
+          }
+        //--------------------------------------------------------------------
+          public void Exercicio5()
+          {
+              //exercicio 5
+              int a = 0;
+              
+              string menssagem = string.Empty;
+              
+              
+              
+              
+            
+                for (int c = 0; c <= 10; c++)
+                {
+                while (a <= c)
+
+                {
+                    menssagem += (c * 5).ToString() + Environment.NewLine;
+                    a++;
+
+                }
+               
+            }
+            MessageBox.Show(menssagem);
+        }
+        //--------------------------------------------------------------------
+          private void Exercicio6()
+          {
+              string tabuada = string.Empty;
+
+              int numeroUsuario = Convert.ToInt32(txttabuada.Text);
+              for (int i = 1; i <= 10; i++)
+              {
+                  
+                  tabuada += (numeroUsuario + " x " + i + "=" +numeroUsuario * i).ToString() + Environment.NewLine;
+              }
+              MessageBox.Show(tabuada);
+          }
+
+        private void Exercicio7()
+        {
+            double valorP = 10, valorV = 20;
+            int nProdutosP = 0, nProdutosV = 0;
+            nProdutosP = (Convert.ToInt32(txtP.Text)); 
+            nProdutosV = (Convert.ToInt32(txtV.Text));
+
+            double valorTotal = ((nProdutosP * valorP) + (nProdutosV * valorV));
+            MessageBox.Show("Valor total é :" + valorTotal);
+             
+
+        }
+
+       
+
+
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Exercicio1();
+        }
+
+        private void btnExercicio2_Click(object sender, EventArgs e)
+        {
+            Exercicio2();
+        }
+        
+        
+        private void btnExercicio3_Click(object sender, EventArgs e)
+        {
+            Exercicio3();
+        }
+
+        private void btnExercicio5_Click(object sender, EventArgs e)
+        {
+            Exercicio5();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Exercicio6();
+        }
+
+        private void txttabuada_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsDigit(e.KeyChar) && !(e.KeyChar == '\b'))
+                e.Handled = true;
+
+        }
+
+        private void btnExercicio4_Click(object sender, EventArgs e)
+        {
+            Exercicio4();
+        }
+
+        private void txtExercicio4_TextChanged(object sender, EventArgs e)
+        {
+        
+        }
+
+        private void btnExercicio7_Click(object sender, EventArgs e)
+        {
+            Exercicio7();
+
+        }
+
+      
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tabPage2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
